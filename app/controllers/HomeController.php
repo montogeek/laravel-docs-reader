c6539d12e59af5e9264ec17179c9efd4ecc29169
Fernando Montoya <montogeek@gmail.com>
2015-01-24 23:19:20 -0800

    Fix redirection bug

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -1,8 +1,8 @@
 <?php
 class HomeController extends BaseController {
 
-	public function showWelcome($version = '4.1')
+	public function getIndex($version = '4.1')
 	{
-		return Redirect::action('Page@show', [$version, 'introduction']);
+		return Redirect::action('Page@getIndex', [$version, 'introduction']);
 	}
 }
