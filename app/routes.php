708cd0e7a52f7882eeab75020d277f8e27f29319
Fernando Montoya <montogeek@gmail.com>
2015-01-28 21:44:38 -0800

    Ignored submodules and added hook, hope it works

--- app/routes.php
+++ app/routes.php
@@ -2,3 +2,6 @@
 
 Route::get('/', 'HomeController@getIndex');
 Route::get('{version}/{page}', 'Page@getIndex');
+Route::post('hook', function() {
+    shell_exec('git submodule foreach git pull');
+});
