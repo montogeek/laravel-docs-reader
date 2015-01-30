6938a663b76df2d81cac2b9ff1aea7d3ab4c8568
Fernando Montoya <montogeek@gmail.com>
2015-01-29 18:43:36 -0800

    Update routes.php

--- app/routes.php
+++ app/routes.php
@@ -4,4 +4,5 @@ Route::get('/', 'HomeController@getIndex');
 Route::get('{version}/{page}', 'Page@getIndex');
 Route::post('hook', function() {
     shell_exec('git submodule foreach git pull');
+    return Response::json(array('success' => true));
 });
