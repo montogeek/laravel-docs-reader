a84137c009461c779df1c39b15ff2cf169a6426b
Fernando Montoya <montogeek@gmail.com>
2015-01-31 02:24:59 -0800

    Update routes.php

--- app/routes.php
+++ app/routes.php
@@ -2,8 +2,3 @@
 
 Route::get('/', 'HomeController@getIndex');
 Route::get('{version}/{page}', 'Page@getIndex');
-Route::post('hook', function() {
-    $temp = shell_exec('git submodule update --remote --merge');
-    Log::info($temp);
-    return Response::json(array('success' => true, 'message' => $temp));
-});
