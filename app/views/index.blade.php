668d69f2b1a22c6bf091f49026384d88e74cd82d
Fernando Montoya <montogeek@gmail.com>
2015-02-14 22:02:06 -0800

    Initial markup

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -1,8 +1,8 @@
 <!doctype html>
-<html lang="es" ng-app="docsApp">
+<html lang="es">
 <head>
   <meta charset="UTF-8">
-  <title>Documentación de Laravel en Español versión 4.1</title>
+  <title>Documentación de Laravel en Español versión 5.0 y 4.x</title>
   <link rel="stylesheet" type="text/css" href="{{ asset_path('styles/main.css') }}">
 </head>
 <body>
@@ -14,6 +14,18 @@
         </a>
         <ul class="right inline-list">
           <li>
+            {{ link_to_action('Page@getIndex', '5.0', ['5.0', 'introduction']) }}
+          </li>
+          <li>
+            {{ link_to_action('Page@getIndex', '4.1', ['4.1', 'introduction']) }}
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div class="row">
+      <div class="columns large-12">
+        <ul class="left inline-list">
+          <li>
             <a href="//laravel.montogeek.co">Inicio</a>
           </li>
           <li>
@@ -24,18 +36,6 @@
           </li>
           <li>
             <a href="https://github.com/montogeek/laravel-docs-es">Ayuda a traducir</li>
-        </ul>
-      </div>
-    </div>
-    <div class="row">
-      <div class="columns large-4">
-        <ul class="left inline-list">
-          <li>
-            {{ link_to_action('Page@getIndex', '5.0', ['5.0', 'introduction']) }}
-          </li>
-          <li>
-            {{ link_to_action('Page@getIndex', '4.1', ['4.1', 'introduction']) }}
-          </li>
         </ul>
       </div>
     </div>
